f547781abab0fa555dff47360e2d54b280ab466d
ythStevin0 <404Stevino@gmail.com>
2026-03-08 22:22:22 +0700

    feat: initial backend setup with Express and MongoDB Atlas

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './src/config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Company Profile API is running");
+});
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
---
+++ backend/src/config/db.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
