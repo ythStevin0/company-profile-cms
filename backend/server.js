5bc77b0ee6a8f17221169db3e5aeebd2e6f2ac8a
ythStevin0 <404Stevino@gmail.com>
2026-03-15 22:40:34 +0700

    feat: register auth routes and project message endpoit with JWT

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import connectDB from './src/config/db.js';
 import serviceRoutes from './src/modules/services/services.routes.js';
 import portfolioRoutes from './src/modules/portfolio/portfolio.routes.js';
 import messageRoutes from './src/modules/messages/messages.routes.js';
+import authRoutes from './src/modules/auth/auth.routes.js';
+import protect from './src/middleware/auth.middleware.js';
 
 dotenv.config();
 
@@ -14,14 +16,14 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/services', serviceRoutes);
+app.use('/api/portfolio', portfolioRoutes);
+app.use('/api/messages', protect, messageRoutes);  
 
 app.get("/", (req, res) => {
     res.send("Company Profile API is running");
 });
-
-app.use('/api/services', serviceRoutes);
-app.use('/api/portfolio', portfolioRoutes);
-app.use('/api/messages', messageRoutes);
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
